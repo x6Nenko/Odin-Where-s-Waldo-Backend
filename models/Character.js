392cca28e4b34364a39466deeed74be579589680
Max I <clickkava@gmail.com>
2024-05-21 23:22:39 +0300

    Change character schema

--- models/Character.js
+++ models/Character.js
@@ -5,13 +5,11 @@ const Schema = mongoose.Schema;
 const CharacterSchema = new Schema({
   name: { type: String, required: true, minLength: 1, maxLength: 50 },
   game: { type: Schema.Types.ObjectId, ref: "Game", required: true },
-  coordinates: {
-    type: {
-      x: { type: Number, required: true },
-      y: { type: Number, required: true }
-    },
-    required: true
-  },
+  coordinates: [{
+    media_query: {type: String, required: true},
+    x: { type: Number, required: true },
+    y: { type: Number, required: true }
+  }]
 });
 
 CharacterSchema.virtual("url").get(function () {
