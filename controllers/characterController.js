af40037f1bbfce7067d326a9c80ce41d4c217675
Max I <clickkava@gmail.com>
2024-05-25 21:15:27 +0300

    Set up character controller
    Validate whether or not the user has clicked the right place for the
    character.

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,10 +1,29 @@
 const Character = require("../models/Character");
+const Game = require("../models/Game");
 
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 
 exports.check_post = asyncHandler(async (req, res, next) => {
+  const selectedChar = req.body.selectedCharacter;
+  const charData = await Character.findOne({name: selectedChar}).populate("game").exec();
 
-  res.json({ title: "POST check" });
+  const coordX = charData.coordinates[0].x;
+  const maxX = coordX + 0.02;
+  const minX = coordX - 0.02;
+
+  const coordY = charData.coordinates[0].y;
+  const maxY = coordY + 0.02;
+  const minY = coordY - 0.02;
+
+  const pickedX = Math.floor(req.body.pickedPosition.x * 100) / 100;
+  const pickedY = Math.floor(req.body.pickedPosition.y * 100) / 100;
+
+  if (pickedX >= minX && pickedX <= maxX && pickedY >= minY && pickedY <= maxY) {
+    return res.json({ match: true });
+  } else {
+    return res.json({ match: false });
+  };
+
 });
---
+++ db.js
@@ -0,0 +1,41 @@
+// +- 2
+// Math.floor(1.0789 * 100) / 100
+
+const chars = [
+  {
+    name: "Guts",
+    game: "Game1",
+    coordinates: 
+    [
+      {
+        media_query: "A",
+        x: 0.49,
+        y: 0.90,
+      },
+    ]
+  },
+  {
+    name: "Griffith",
+    game: "Game1",
+    coordinates: 
+    [
+      {
+        media_query: "A",
+        x: 0.05,
+        y: 0.47,
+      },
+    ]
+  },
+  {
+    name: "Crash",
+    game: "Game1",
+    coordinates: 
+    [
+      {
+        media_query: "A",
+        x: 0.52,
+        y: 0.58,
+      },
+    ]
+  }
+]
